d6a252f93c59ea6d71d3bffec1fa2e172166d2f7
chaulotj <chaulotj@gmail.com>
2020-10-04 16:26:14 -0400

    Did a bunch of crap to make it work and started my minigame. (But only a little bit)

---
+++ Project 2/Assets/Scripts/LightSceneManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSceneManager : MonoBehaviour
+{
+    public LightIngredient ingredient;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(ingredient, Vector3.zero, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
